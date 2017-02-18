7997a92e1c1a7b6d461cee1cd07b49953161d5f9
Juan Ignacio Volpe <juanignaciov96@gmail.com>
2017-02-18 14:55:34 -0300

    Se lee directorio test para solo testear y reportar coverage a sources con respectivos tests

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,37 @@ var inject = require('gulp-inject');
 var istanbul = require('gulp-istanbul');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
 var istanbulReport = require('gulp-istanbul-report');
+var fs = require('fs');
+
+var walkSync = function(dir) {
+	var filelist = [];
+	var fs = fs || require('fs'),
+	files = fs.readdirSync(dir);
+	filelist = filelist || [];
+	files.forEach(function(file) {
+		if (fs.statSync(dir + file).isDirectory()) {
+			filelist = walkSync(dir + file + '/', filelist);
+		}
+		else {
+			if(file.indexOf("Test") !== -1) {
+				var srcName ="./src/" +  file.replace(/test/i,"");
+				filelist.push(srcName);
+			}
+		}
+	});
+	return filelist;
+};
 
 gulp.task('instrument', function () {
-	return gulp.src(['src/*.js'])
+	var paths = walkSync("./tests/");
+
+	return gulp.src(paths)
 	// Covering files
 		.pipe(istanbul({
 			coverageVariable: '__coverage__'
 		}))
 		// instrumented files will go here
-		.pipe(gulp.dest('tests/instrumented/'));
+		.pipe(gulp.dest('coverage/instrumented/'));
 });
 
 
@@ -37,9 +59,10 @@ gulp.task('coverage', ['instrument', 'inject'], function () {
 
 gulp.task('inject', ['instrument'], function (cb) {
     var paths = {
-    	"javascript": ["tests/instrumented/*.js"],
+    	"javascript": ["coverage/instrumented/*.js"],
     	tests: ["tests/*.js"]
     };
+
 	return gulp.src('testRunner.html')
 		.pipe(inject(
 			gulp.src(paths.javascript,{read: false}), {
@@ -54,12 +77,12 @@ gulp.task('inject', ['instrument'], function (cb) {
 });
 
 gulp.task('injectRunner', function (cb) {
-    var paths = {
-    	"javascript": ["src/*.js"],
-    	tests: ["tests/*.js"]
-    };
-
-	return gulp.src('index.html')
+	var paths = {
+		"javascript": [],
+		tests: ["tests/*.js"]
+	};
+	paths.javascript = walkSync("./tests/");
+	return gulp.src('testRunner.html')
 		.pipe(inject(
 			gulp.src(paths.javascript,{read: false}), {
 				relative: true
